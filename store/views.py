7516d77100cb63256f92cfa12f5286b7b29cf8ff
chins <chinsula81@gmail.com>
2023-12-17 12:10:07 +0500

    my changes 17.12.2023

--- store/views.py
+++ store/views.py
@@ -6,13 +6,15 @@ from django.http import HttpResponse, HttpResponseNotFound
 def products_view(request):
     if request.method == "GET":
         id_product = request.GET.get('id')
+        if not id_product:
+            return JsonResponse(DATABASE, json_dumps_params={'ensure_ascii': False,
+                                                     'indent': 4})# Вернуть JsonResponse с объектом DATABASE и параметрами отступов и кодировок,
         if id_product in DATABASE:
             return JsonResponse(DATABASE[id_product], json_dumps_params={'ensure_ascii': False,
                                                      'indent': 4})
         if id_product not in DATABASE:
             return HttpResponseNotFound("Данного продукта нет в базе данных")
-        return JsonResponse(DATABASE, json_dumps_params={'ensure_ascii': False,
-                                                     'indent': 4})# Вернуть JsonResponse с объектом DATABASE и параметрами отступов и кодировок,
+
 def shop_view(request):
     if request.method == "GET":
         with open('store/shop.html', encoding="utf-8") as f:

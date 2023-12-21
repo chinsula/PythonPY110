e2ae601f618ef7449fa0473d09f0c228ec9e8313
chins <chinsula81@gmail.com>
2023-12-21 17:14:36 +0500

    перенос блоков из shop в base

--- store/views.py
+++ store/views.py
@@ -54,17 +54,18 @@ def cart_view(request):
     if request.method == "GET":
         data = view_in_cart()
         if request.GET.get('format') == 'JSON':
-            return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
-                                                         products = []  # Список продуктов
+            return JsonResponse(data, json_dumps_params={'ensure_ascii': False,'indent': 4})
+
+        products = []  # Список продуктов
         for product_id, quantity in data['products'].items():
             product = DATABASE[product_id]  # 1. Получите информацию о продукте из DATABASE по его product_id. product будет словарём
-        # 2. в словарь product под ключом "quantity" запишите текущее значение товара в корзине
-        product["quantity"] = quantity
-            "price_total"] = f"{quantity * product['price_after']:.2f}"  # добавление общей цены позиции с ограничением в 2 знака 
-        # 3. добавьте product в список products
+            # 2. в словарь product под ключом "quantity" запишите текущее значение товара в корзине
+            product["quantity"] = quantity
+            product["price_total"] = f"{quantity * product['price_after']:.2f}"  # добавление общей цены позиции с ограничением в 2 знака
+            # 3. добавьте product в список products
+            products.append(product)
 
-                                 'indent': 4})
-        return render(request, "store/cart.html")
+        return render(request, "store/cart.html", context={"products": products})
 
 def cart_add_view(request, id_product):
     if request.method == "GET":
